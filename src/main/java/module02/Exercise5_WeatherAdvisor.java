36089576c6cc58b3ba7c9759c08db3892802d7ca
Benit Polvie <polviedivine@gmail.com>
2026-05-09 16:44:09 +0200

    Create Exercise5_WeatherAdvisor.java

---
+++ src/main/java/module02/Exercise5_WeatherAdvisor.java
@@ -0,0 +1,11 @@
+// module02/Exercise5_WeatherAdvisor.java
+package module02;
+
+public class Exercise5_WeatherAdvisor {
+    public static void main(String[] args) {
+        int temperature = 35;
+
+        // Write your code below
+
+    }
+}
