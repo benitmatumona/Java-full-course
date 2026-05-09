a41d12ec2fd86ef4294025c07a1ccd733188caaa
Benit Polvie <polviedivine@gmail.com>
2026-05-09 16:43:04 +0200

    Create Exercise3_EvenOrOdd.java

---
+++ src/main/java/module02/Exercise3_EvenOrOdd.java
@@ -0,0 +1,11 @@
+// module02/Exercise3_EvenOrOdd.java
+package module02;
+
+public class Exercise3_EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 7;
+
+        // Write your code below
+
+    }
+}
