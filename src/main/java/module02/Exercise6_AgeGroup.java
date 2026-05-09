01a374c349f33e66982d038807aff7daced0c2ab
Benit Polvie <polviedivine@gmail.com>
2026-05-09 16:45:41 +0200

    Create Exercise6_AgeGroup.java

---
+++ src/main/java/module02/Exercise6_AgeGroup.java
@@ -0,0 +1,11 @@
+// module02/Exercise6_AgeGroup.java
+package module02;
+
+public class Exercise6_AgeGroup {
+    public static void main(String[] args) {
+        int age = 15;
+
+        // Write your code below
+
+    }
+}
