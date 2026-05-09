1443bf486903cfd71cf9360dcc7c7d294b4dead0
Benit Polvie <polviedivine@gmail.com>
2026-05-09 15:46:41 +0200

    Update CollectionsExercises.java

--- src/main/java/module11/CollectionsExercises.java
+++ src/main/java/module11/CollectionsExercises.java
@@ -39,4 +39,47 @@ public class CollectionsExercises {
     public static String exercise7_gradeDescription(Grade grade) {
         return null;
     }
+
+    // Exercise 8
+    public static ArrayList<Integer> exercise8_reverseList(ArrayList<Integer> list) {
+        return null;
+    }
+
+    // Exercise 9
+    public static ArrayList<Integer> exercise9_findDuplicates(ArrayList<Integer> list) {
+        return null;
+    }
+
+    // Exercise 10
+    public static HashMap<String, Integer> exercise10_mergeMaps(
+            HashMap<String, Integer> map1,
+            HashMap<String, Integer> map2) {
+        return null;
+    }
+
+    // Exercise 11
+    public static String exercise11_findHighestValueKey(HashMap<String, Integer> map) {
+        return null;
+    }
+
+    // Exercise 12
+    public static HashMap<Integer, ArrayList<String>> exercise12_groupWordsByLength(
+            ArrayList<String> words) {
+        return null;
+    }
+
+    // Exercise 13
+    public static HashMap<Integer, String> exercise13_invertMap(HashMap<String, Integer> map) {
+        return null;
+    }
+
+    // Exercise 14
+    public static ArrayList<String> exercise14_removeNulls(ArrayList<String> list) {
+        return null;
+    }
+
+    // Exercise 15
+    public static HashMap<Grade, Integer> exercise15_countGrades(ArrayList<Grade> grades) {
+        return null;
+    }
 }
